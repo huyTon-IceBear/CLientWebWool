9a1293c19706b753c2eb6ae5a140d41c9763c55a
Huy Ton <5497060-ramenbowl@users.noreply.gitlab.com>
2023-01-10 00:19:00 +0100

    add config for port & baseUrl

--- WoolLib/replies/reply.js
+++ WoolLib/replies/reply.js
@@ -1,32 +1,34 @@
 let content, node, interactionId, dialogueId;
-
+import { config } from '../config.js';
 class Reply extends HTMLElement {
   constructor() {
     super();
-    let replies = document.createElement("div");
-    replies.setAttribute("class", "reply-container");
+    this.port = config.port;
+    this.baseUrl = config.baseUrl;
+    let replies = document.createElement('div');
+    replies.setAttribute('class', 'reply-container');
 
-    const text = JSON.parse(this.getAttribute("data"));
+    const text = JSON.parse(this.getAttribute('data'));
     content = text.value?.replies || text.replies;
     console.log(content);
     node = text.value?.node || text.node;
     interactionId =
       text.value?.loggedInteractionIndex || text.loggedInteractionIndex;
     dialogueId = text.value?.loggedDialogueId || text.loggedDialogueId;
-    replies.setAttribute("id", interactionId);
+    replies.setAttribute('id', interactionId);
 
     if (content !== null) {
       for (let i = 0; i < content.length; i++) {
-        const r = content[i].statement?.segments[0].text || "Continue";
+        const r = content[i].statement?.segments[0].text || 'Continue';
         replies.innerHTML += `<p class="${node}-${interactionId}-reply${i}">${r}</p>`;
       }
     }
 
-    const linkElem = document.createElement("link");
-    linkElem.setAttribute("rel", "stylesheet");
-    linkElem.setAttribute("href", "../../WoolLib/replies/reply.css");
+    const linkElem = document.createElement('link');
+    linkElem.setAttribute('rel', 'stylesheet');
+    linkElem.setAttribute('href', '../../WoolLib/replies/reply.css');
 
-    const shadowRoot = this.attachShadow({ mode: "open" });
+    const shadowRoot = this.attachShadow({ mode: 'open' });
     shadowRoot.appendChild(replies);
     shadowRoot.appendChild(linkElem);
   }
@@ -36,45 +38,45 @@ class Reply extends HTMLElement {
       const a = this.shadowRoot.querySelector(
         `.${node}-${interactionId}-reply${i}`
       );
-      a.setAttribute("reply-id", content[i].replyId);
-      a.setAttribute("end-or-not", content[i].endsDialogue);
-      a.setAttribute("interaction", interactionId);
+      a.setAttribute('reply-id', content[i].replyId);
+      a.setAttribute('end-or-not', content[i].endsDialogue);
+      a.setAttribute('interaction', interactionId);
 
-      a.addEventListener("click", this.progress.bind(this));
+      a.addEventListener('click', this.progress.bind(this));
     }
   }
 
   async progress(e) {
     e.preventDefault();
-    let id = parseInt(e.target.getAttribute("reply-id"));
-    let end = e.target.getAttribute("end-or-not");
+    let id = parseInt(e.target.getAttribute('reply-id'));
+    let end = e.target.getAttribute('end-or-not');
     let sameStep =
-      parseInt(e.target.getAttribute("interaction")) == interactionId;
+      parseInt(e.target.getAttribute('interaction')) == interactionId;
 
     if (sameStep) {
-      if (end === "true") {
+      if (end === 'true') {
         convoContainer.insertAdjacentHTML(
-          "beforeend",
+          'beforeend',
           `<div class="end-dialogue">End Dialogue.</div>`
         );
         interactionId = -1;
       } else {
         const input = `<div class="user-data"><p class="user-${id}">${e.target.innerHTML}</p><agent-avatar></agent-avatar></div>`;
-        convoContainer.insertAdjacentHTML("beforeend", input);
+        convoContainer.insertAdjacentHTML('beforeend', input);
 
         await $.ajax({
-          url: "http://localhost:8080/wool/v1/dialogue/progress",
-          type: "POST",
-          dataType: "json",
+          url: this.baseUrl + port + '/wool/v1/dialogue/progress',
+          type: 'POST',
+          dataType: 'json',
           data: {
             loggedDialogueId: dialogueId,
             loggedInteractionIndex: interactionId,
             replyId: id,
           },
-          headers: { "X-Auth-Token": sessionStorage.authToken },
+          headers: { 'X-Auth-Token': sessionStorage.authToken },
           success: function (res) {
             this.dispatchEvent(
-              new CustomEvent("data-received", { detail: res })
+              new CustomEvent('data-received', { detail: res })
             );
             // return res;
           }.bind(this),
